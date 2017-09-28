ddebdc92145d57707c9d2f4bebcd1169800ce864
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-28 22:16:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,12 @@ def line(katz_deli)
 if katz_deli.length == 0
   puts "The line is currently empty."
 else
-  list = katz_deli.collect {|name, index| "The line is currently: #{index + 1}. #{name}"}
-  list
+  list = []
+  katz_deli.each_with_index do |name, i|
+   list = katz_deli.push("The line is currently: #{index + 1}. #{name}")
+ end
+ return list
 
-end
 end
 
 def take_a_number(katz_deli, name)
