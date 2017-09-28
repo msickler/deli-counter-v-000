ed1200429a6e9d3480eb7e8a0c0d1c3a098db588
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-28 22:12:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,9 @@ def line(katz_deli)
 if katz_deli.length == 0
   puts "The line is currently empty."
 else
-  katz_deli.each { |name|  }
+  list = katz_deli.collect {|name, index| "The line is currently: #{{index + 1}}. #{{name}}"}
+ return list
+  
 end
 end
 
